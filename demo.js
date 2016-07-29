1cb4c38598d4205d7cb6f7ebd04623d41a7f4ac9
ranjith1237 <ranjithreddy1061995@gmail.com>
2016-07-29 12:17:28 +0530

    added basic auth

--- demo.js
+++ demo.js
@@ -6,8 +6,10 @@ var fileUpload = require('express-fileupload');
 var morgan = require('morgan');
 var app = express();
 var bodyParser = require('body-parser');
+var auth = require('basic-auth');
 app.use(fileUpload());
 app.use(morgan('dev'));
+
 
 
 app.use(bodyParser.urlencoded({
@@ -17,28 +19,69 @@ app.use(bodyParser.json());
 
 
 app.get("/nodeabl/:fname*",function(req,res){
-	 nodesimpleabl.getABL(req.url,res);
+	var user = auth(req);
+	if(user.name=='progress'&&user.pass=='Progress@123456')
+	{
+	 	nodesimpleabl.getABL(req.url,res);
+	}
+	else
+	{
+		res.send('Enter credentials correctly');
+	}
 });
 
 app.post("/nodeabl/:fname*",function(req,res){
-	var response = nodesimpleabl.postABL(req.url,req.body);
-	res.send(response);
+	var user = auth(req);
+	if(user.name=='progress'&&user.pass=='Progress@123456')
+	{
+		var response = nodesimpleabl.postABL(req.url,req.body);
+		res.send(response);
+	}
+	else
+	{
+		res.send('Enter credentials correctly');
+	}
+
 });
 
 
 app.put("/nodeabl/:fname*",function(req,res){
-	var response = nodesimpleabl.putABL(req.url,req.body);
-	res.send(response);
+	var user = auth(req);
+	if(user.name=='progress'&&user.pass=='Progress@123456')
+	{
+		var response = nodesimpleabl.putABL(req.url,req.body);
+		res.send(response);	
+	}
+	else
+	{
+		res.send('Enter credentials correctly');
+	}
 });
 
 app.delete("/nodeabl/:fname*",function(req,res){
-	var response = nodesimpleabl.delABL(req.url,req.body);
-	res.send(response);
+	var user = auth(req);
+	if(user.name=='progress'&&user.pass=='Progress@123456')
+	{
+		var response = nodesimpleabl.delABL(req.url,req.body);
+		res.send(response);
+	}
+	else
+	{
+		res.send('Enter credentials correctly');
+	}
 });
 
 app.post("/publish",function(req,res){
-	var response = nodesimpleabl.upload_file(req.files);
-	res.send(response);
+	var user = auth(req);
+	if(user.name=='progress'&&user.pass=='Progress@123456')
+	{
+		var response = nodesimpleabl.upload_file(req.files);
+		res.send(response);
+	}
+	else
+	{
+		res.send('Enter credentials correctly');
+	}	
 });
 
 var server = app.listen(8081, function () {
